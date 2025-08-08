b9317c6c397325804505a84e8271e819b5e6642b
Reza Mohajerpoor <rezamohajerpoor@Rezas-MacBook-Pro.local>
2025-08-08 23:52:01 +1000

    also modified the first course Build rag applications to run locally

--- Build_rag_applications/basic_rag_app/simple_llm.py
+++ Build_rag_applications/basic_rag_app/simple_llm.py
@@ -1,34 +1,35 @@
-# Import the necessary packages
-from ibm_watsonx_ai.foundation_models import ModelInference
-from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
-from ibm_watsonx_ai import Credentials
-from langchain_ibm import WatsonxLLM
+#%% Import the necessary packages
+
+import groq
+from groq import Groq
+from langchain_groq import ChatGroq
+from langchain_mistralai import MistralAIEmbeddings
+import os
+from dotenv import load_dotenv
+
 import gradio as gr
 
-# Specify the model and project settings 
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+mistral_api_key = os.getenv("MISTRAL_API_KEY")
+
+#%% Specify the model and project settings 
 # (make sure the model you wish to use is commented out, and other models are commented)
 #model_id = 'mistralai/mixtral-8x7b-instruct-v01' # Specify the Mixtral 8x7B model
-model_id = 'ibm/granite-3-2-8b-instruct' # Specify IBM's Granite 3.3 8B model
+model_id = "llama3-8b-8192" # Specify IBM's Granite 3.3 8B model
 
-# Set the necessary parameters
-parameters = {
-    GenParams.MAX_NEW_TOKENS: 256,  # Specify the max tokens you want to generate
-    GenParams.TEMPERATURE: 0.5, # This randomness or creativity of the model's responses
-}
+llm = ChatGroq(model=model_id,
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        verbose=1)
 
-project_id = "skills-network"
-
-# Wrap up the model into WatsonxLLM inference
-watsonx_llm = WatsonxLLM(
-    model_id=model_id,
-    url="https://us-south.ml.cloud.ibm.com",
-    project_id=project_id,
-    params=parameters,
-)
-
+#%%
 # Function to generate a response from the model
 def generate_response(prompt_txt):
-    generated_response = watsonx_llm.invoke(prompt_txt)
+    generated_response = llm.invoke(prompt_txt).content
     return generated_response
 # Create Gradio interface
 chat_application = gr.Interface(
--- Build_rag_applications/rag_app_icebreaker/config.py
+++ Build_rag_applications/rag_app_icebreaker/config.py
@@ -5,8 +5,8 @@ WATSONX_URL = "https://us-south.ml.cloud.ibm.com"
 WATSONX_PROJECT_ID = "skills-network"
 
 # Model settings
-LLM_MODEL_ID = "ibm/granite-3-2-8b-instruct"
-EMBEDDING_MODEL_ID = "ibm/slate-125m-english-rtrvr"
+LLM_MODEL_ID = "llama3-8b-8192"
+EMBEDDING_MODEL_ID = "mistral-embed"
 
 # ProxyCurl API settings
 PROXYCURL_API_KEY = ""  # Replace with your API key
